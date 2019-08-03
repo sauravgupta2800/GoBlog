f6019a20e5ac98ebd4679811f3709bdea25026cd
Saurav Gupta <saurav.gupta@sprinkledata.com>
2019-08-03 19:28:43 +0530

    Adding running modules

---
+++ flaskblog/app/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='4df0ad67cbc5f93fdf9abea8938a082b'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager= LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config["MAIL_SERVER"]='smtp.googlemail.com'
+app.config["MAIL_PORT"]=587
+app.config["MAIL_USE_TLS"]=True
+app.config["MAIL_USERNAME"]='sauravgupta2800@gmail.com'
+app.config["MAIL_PASSWORD"]='Shivansh11602832@10'
+
+mail = Mail(app)
+
+from app import routes  
