a767961637e80d109d9c4b2398e40bb9f2a6e261
Saurav Gupta <sauravgupta2800@gmail.com>
2019-08-12 10:53:39 +0530

    testing check

--- flaskblog/app/users/utils.py
+++ flaskblog/app/users/utils.py
@@ -9,7 +9,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _ , f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn) # method called
     form_picture.save(picture_path)
 
     return picture_fn
